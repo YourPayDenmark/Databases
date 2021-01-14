661d3131bdf477d0013284f30049a38851bc20a7
MathiasGajhede <mag@yourpay.io>
2021-01-14 08:25:19 +0100

    Update 90.app_user_pricing.sql

--- sql-changes/90.app_user_pricing.sql
+++ sql-changes/90.app_user_pricing.sql
@@ -3,3 +3,4 @@
 --
 USE dinterminal;
 ALTER TABLE `app_user_pricing` ADD `version` SMALLINT(1) NOT NULL DEFAULT '1' COMMENT 'Version 1 = Deprecated invoices pre 2021' AFTER `user_pricing_id`;
+ALTER TABLE `app_user_pricing` ADD `product_lines` LONGTEXT NOT NULL AFTER `next_stopped`;
