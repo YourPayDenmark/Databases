1e2d8e5e06eae946e85aa5c1ed38b98e9eed202c
MathiasGajhede <mag@yourpay.io>
2021-03-26 00:09:16 +0100

    Create 10-psp_settings.sql

---
+++ sql-changes/10-psp_settings.sql
@@ -0,0 +1,12 @@
+use dinterminal;
+
+CREATE TABLE `psp_settings` (
+  `psp_id` int(8) NOT NULL,
+  `psp_setting` varchar(255) NOT NULL,
+  `psp_value` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `psp_system_urls` (
+  `pspid` int(7) NOT NULL,
+  `psp_url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
