5bc60f9ce40d66dc410d496c5f9397d029d4ad89
EmilYourpay <eej@yourpay.io>
2021-04-08 13:07:24 +0200

    Update partner_terminal_orders.sql

--- sql-changes/partner_terminal_orders.sql
+++ sql-changes/partner_terminal_orders.sql
@@ -1,32 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.9.4
--- https://www.phpmyadmin.net/
---
--- Vært: aurora-1.cluster-czl14h06ail9.eu-west-1.rds.amazonaws.com
--- Genereringstid: 08. 04 2021 kl. 07:38:42
--- Serverversion: 5.6.10
--- PHP-version: 7.0.33
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `dinterminal`
---
-
--- --------------------------------------------------------
-
---
--- Struktur-dump for tabellen `partner_terminal_orders`
---
+USE `dinterminal`
 
 CREATE TABLE `partner_terminal_orders` (
   `order_id` int(11) NOT NULL,
@@ -34,7 +6,3 @@ CREATE TABLE `partner_terminal_orders` (
   `qty` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
