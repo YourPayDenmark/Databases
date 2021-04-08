0f5c40439c55801ce8a3466565d72e2f51e43cd6
EmilYourpay <eej@yourpay.io>
2021-04-08 13:07:49 +0200

    Update partner_product_list.sql

--- sql-changes/partner_product_list.sql
+++ sql-changes/partner_product_list.sql
@@ -1,32 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.9.4
--- https://www.phpmyadmin.net/
---
--- Vært: aurora-1.cluster-czl14h06ail9.eu-west-1.rds.amazonaws.com
--- Genereringstid: 08. 04 2021 kl. 07:38:05
--- Serverversion: 5.6.10
--- PHP-version: 7.0.33
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `dinterminal`
---
-
--- --------------------------------------------------------
-
---
--- Struktur-dump for tabellen `partner_product_list`
---
+USE `dinterminal`
 
 CREATE TABLE `partner_product_list` (
   `product_id` int(11) NOT NULL,
@@ -36,17 +8,6 @@ CREATE TABLE `partner_product_list` (
   `currency` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Begrænsninger for dumpede tabeller
---
-
---
--- Indeks for tabel `partner_product_list`
---
 ALTER TABLE `partner_product_list`
   ADD PRIMARY KEY (`product_id`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
