742aefc1f46640e3754f20b707190d4cad1ccf7b
EmilYourpay <eej@yourpay.io>
2021-04-08 13:05:29 +0200

    Update partner_transfer_accounts.sql

--- sql-changes/partner_transfer_accounts.sql
+++ sql-changes/partner_transfer_accounts.sql
@@ -1,32 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.9.4
--- https://www.phpmyadmin.net/
---
--- Vært: aurora-1.cluster-czl14h06ail9.eu-west-1.rds.amazonaws.com
--- Genereringstid: 08. 04 2021 kl. 07:37:35
--- Serverversion: 5.6.10
--- PHP-version: 7.0.33
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `dinterminal`
---
-
--- --------------------------------------------------------
-
---
--- Struktur-dump for tabellen `partner_transfer_accounts`
---
+USE `dinterminal`
 
 CREATE TABLE `partner_transfer_accounts` (
   `accountid` bigint(12) DEFAULT NULL,
@@ -64,6 +36,3 @@ CREATE TABLE `partner_transfer_accounts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
