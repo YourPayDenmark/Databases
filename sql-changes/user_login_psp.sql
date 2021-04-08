6bbcc3462dbd90fe60b869a9d06b742192d629be
EmilYourpay <eej@yourpay.io>
2021-04-08 09:46:22 +0200

    Create user_login_psp.sql

---
+++ sql-changes/user_login_psp.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.4
+-- https://www.phpmyadmin.net/
+--
+-- Vært: aurora-1.cluster-czl14h06ail9.eu-west-1.rds.amazonaws.com
+-- Genereringstid: 08. 04 2021 kl. 07:40:17
+-- Serverversion: 5.6.10
+-- PHP-version: 7.0.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dinterminal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur-dump for tabellen `user_login_psp`
+--
+
+CREATE TABLE `user_login_psp` (
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `session_id` varchar(255) DEFAULT NULL,
+  `access_rights` varchar(255) DEFAULT NULL,
+  `language` varchar(255) DEFAULT NULL,
+  `pspid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Begrænsninger for dumpede tabeller
+--
+
+--
+-- Indeks for tabel `user_login_psp`
+--
+ALTER TABLE `user_login_psp`
+  ADD PRIMARY KEY (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
