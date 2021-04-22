8d091a3a508cd5f5f58a55eb2c91a266fd798f32
EmilYourpay <eej@yourpay.io>
2021-04-22 09:35:28 +0200

    Update user_login_psp.sql

--- sql-changes/user_login_psp.sql
+++ sql-changes/user_login_psp.sql
@@ -13,12 +13,12 @@ CREATE TABLE `user_login_psp` (
   `language` varchar(255) DEFAULT NULL,
   `pspid` int(11) DEFAULT NULL,
   `reset_token` varchar(255) DEFAULT NULL,
-  `reset_token_expiry` int(10) DEFAULT NULL,
+  `reset_token_expiry` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 INSERT INTO `user_login_psp` (`password`, `name`, `phone`, `email`, `user_id`, `session_id`, `language`, `pspid`, `reset_token`, `reset_token_expiry`, `secret`) VALUES
-('', NULL, NULL, '', 1000000,  NULL, NULL, NULL, NULL, NULL, NULL);
+('', NULL, NULL, '', 1000000,  NULL, NULL, NULL, NULL, NULL, '');
 ​
 
 ALTER TABLE `user_login_psp`
