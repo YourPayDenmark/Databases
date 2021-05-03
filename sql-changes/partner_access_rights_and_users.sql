7d93a67d8497e8741abfc1bbfe8282d31c3f6e4d
JesperYourpay <jsf@yourpay.io>
2021-05-03 11:13:02 +0200

    Update partner_access_rights_and_users.sql

--- sql-changes/partner_access_rights_and_users.sql
+++ sql-changes/partner_access_rights_and_users.sql
@@ -8,6 +8,7 @@ CREATE TABLE `user_login_psp` (
   `email` varchar(255) NOT NULL,
   `user_id` int(11) NOT NULL,
   `session_id` varchar(255) DEFAULT NULL,
+  `access_rights` varchar(255) DEFAULT NULL,
   `language` varchar(255) DEFAULT NULL,
   `pspid` int(11) DEFAULT NULL,
   `reset_token` varchar(255) DEFAULT NULL,
@@ -15,8 +16,8 @@ CREATE TABLE `user_login_psp` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-INSERT INTO `user_login_psp` (`password`, `name`, `phone`, `email`, `user_id`, `session_id`, `language`, `pspid`, `reset_token`, `reset_token_expiry`, `secret`) VALUES
-('', NULL, NULL, '', 1000000,  NULL, NULL, NULL, NULL, NULL, '');
+INSERT INTO `user_login_psp` (`password`, `name`, `phone`, `email`, `user_id`, `session_id`, `access_rights`, `language`, `pspid`, `reset_token`, `reset_token_expiry`, `secret`) VALUES
+('', NULL, NULL, '', 1000000,  NULL,  NULL, NULL, NULL, NULL, NULL, '');
 
 ALTER TABLE `user_login_psp` ADD PRIMARY KEY(`user_id`);
 
