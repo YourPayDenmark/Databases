02eab140a3709c3d0ad54f65a906bf7185499849
EmilYourpay <eej@yourpay.io>
2021-05-05 14:47:21 +0200

    Update crm_login_psp_default_values.sql

--- sql-changes/crm_login_psp_default_values.sql
+++ sql-changes/crm_login_psp_default_values.sql
@@ -1,15 +1,7 @@
 USE dinterminal
 
 ALTER TABLE crm_login_psp
-ALTER terminal_buy_price SET DEFAULT '260000';
-
-ALTER TABLE crm_login_psp
-ALTER terminal_buy_currency SET DEFAULT '208';
-
-ALTER TABLE crm_login_psp
-ALTER currency SET DEFAULT '208';
-
-ALTER TABLE crm_login_psp
-ALTER terminal_buy_price SET DEFAULT '260000';
-
+	ALTER COLUMN terminal_buy_currency SET DEFAULT '208',
+	ALTER COLUMN currency SET DEFAULT '208',
+	ALTER COLUMN terminal_buy_price SET DEFAULT '360000';
 COMMIT;
